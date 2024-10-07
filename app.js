a2b7bf42b822d2a362182e19dacbaf1c6e6fcc9e
CaioAndreta <caioandreta@gmail.com>
2024-10-07 19:58:57 -0300

    mudada porta do heroku

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ app.use(express.static("./public"));
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+console.log(port)
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`server is listening on port ${port}`));
+    app.listen(process.env.PORT || 5000, console.log(`server is listening on port ${port}`));
   } catch (error) {
     console.log(error);
   }
